238b317798c62a35f7fbe02c688c36d008a04d7c
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-09-07 15:17:24 +0800

    first commit

---
+++ src/main/java/com/example/moviespring/MovieSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSpringApplication.class, args);
+	}
+
+}
